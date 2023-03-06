956c79183433001dcd22a93bbf0b67ff24e71ab5
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-06 23:25:18 +0100

    Created application skeleton

---
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/AuctionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atlantbh.auctionappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionAppBackendApplication.class, args);
+	}
+
+}
---
+++ auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/AuctionAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlantbh.auctionappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
