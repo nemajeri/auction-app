278f0a05ca2d4d03f0021ef2d5bc57f5bf4d4096
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-14 16:42:21 +0100

    Added white navbar feature

---
+++ auction-app-frontend/src/assets/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ImHammer2 } from 'react-icons/im';
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <ImHammer2 className='logo__icon' />
+      <h1>AUCTION</h1>
+    </div>
+  );
+};
+
+export default Logo;
--- auction-app-frontend/src/layout/navbar/Navbar.jsx
+++ auction-app-frontend/src/layout/navbar/Navbar.jsx
@@ -1,29 +1,58 @@
 import React, { useState } from 'react';
 import { SocialMediaIcons } from '../../components/social-icons/SocialMediaIcons';
 import './navbar.css';
+import Logo from '../../assets/Logo';
+import SearchField from '../../components/search-field/SearchField';
+import NavbarLink from '../../components/nav-link/NavbarLink';
+import navlinks from '../../data/navlinks.json';
 
 const Navbar = () => {
   const [user, setUser] = useState('');
+  const [activeLink, setActiveLink] = useState('');
+
+  const onClick = (link) => {
+    setActiveLink(link);
+  };
 
   return (
-    <div className='navbar__black'>
-      <div className='navbar__container'>
-        <div>
-          <SocialMediaIcons />
-        </div>
-        {user === '' ? (
-          <div className='navbar__account'>
-            <a href='/temp/route'>Login</a>
-            <span>or</span>
-            <a href='/temp/route'>Create an Account</a>
-          </div>
-        ) : (
+    <>
+      <div className='navbar__black'>
+        <div className='navbar__container'>
           <div>
-            <p>Hi, {user}</p>
+            <SocialMediaIcons />
           </div>
-        )}
+          {user === '' ? (
+            <div className='navbar__account'>
+              <a href='/temp/route'>Login</a>
+              <span>or</span>
+              <a href='/temp/route'>Create an Account</a>
+            </div>
+          ) : (
+            <div>
+              <p>Hi, {user}</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className='navbar__white'>
+        <div className='navbar__container--white'>
+          <Logo />
+          <div className='navbar__container--search_and-links'>
+            <SearchField />
+            <div className='navbar__navigation'>
+              {navlinks.map((link) => (
+                <NavbarLink
+                  link={link}
+                  activeLink={activeLink}
+                  onClick={onClick}
+                  key={link.key}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
