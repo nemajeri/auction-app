92485a6997b9c13b559f7ff69667941b5ccd2890
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-21 08:20:02 +0100

    Changed button styling logic for better reusability

--- auction-app-frontend/src/components/landing-page/highlighted-product/HighlightedProduct.jsx
+++ auction-app-frontend/src/components/landing-page/highlighted-product/HighlightedProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './highlightedProduct.css';
-import Button from '../../../utils/button/Button';
+import Button from '../../../utils/Button';
 
 const HighlightedProduct = ({
   highlightedProduct: { name, price, description, image },
@@ -15,7 +15,7 @@ const HighlightedProduct = ({
         <h2>{name}</h2>
         <span>Start From {price}$</span>
         <p>{description}</p>
-        <Button children={'BID NOW'} onClick={handleButtonClicked} />
+        <Button children={'BID NOW'} onClick={handleButtonClicked} className={'highlighted-product__button'}/>
       </div>
       <img src={image} alt='highlighted-product' />
     </div>
--- auction-app-frontend/src/utils/Button.jsx
+++ auction-app-frontend/src/utils/Button.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BsChevronRight } from 'react-icons/bs';
-import './button.css';
 
-const Button = ({ onClick, children }) => {
+const Button = ({ onClick, children, className }) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick} className={className}>
       <span>
         <h3>{children}</h3> <BsChevronRight />
       </span>
