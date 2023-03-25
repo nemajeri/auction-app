b0c0918e594f2714f7e89915902a68199038466d
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-25 15:37:34 +0100

    Additional repository changes and additions

---
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.atlantbh.auctionappbackend.repository;
+
+import com.atlantbh.auctionappbackend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository()
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findBySubcategoryId(Long subcategoryId);
+}
--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/SubcategoryRepository.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/SubcategoryRepository.java
@@ -1,14 +1,2 @@
-package com.atlantbh.auctionappbackend.repository;
-
-import com.atlantbh.auctionappbackend.model.Subcategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubcategoryRepository extends JpaRepository<Subcategory, Long> {
-    @Query(value = "SELECT COUNT(*) FROM product WHERE subcategory_id = :subcategoryId", nativeQuery = true)
-    int countItemsForSubcategory(@Param("subcategoryId") Long subcategoryId);
+package com.atlantbh.auctionappbackend.repository;public interface SubcategoryRepository {
 }
-
