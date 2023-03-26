ab90d0c74235b679b31a226647b8f9ae667140e3
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-27 00:18:57 +0200

    Changed return type of the method

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/SubcategoryRepository.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/SubcategoryRepository.java
@@ -10,5 +10,5 @@ import java.util.Set;
 
 @Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, Long> {
-    Set<SubcategoryDTO> findAllByCategory(Category category);
+    Set<Subcategory> findAllByCategory(Category category);
 }
