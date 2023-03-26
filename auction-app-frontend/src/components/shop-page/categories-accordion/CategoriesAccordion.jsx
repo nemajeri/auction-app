e752a84e19546d327840bd94c8c53e5c69d61fef
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-26 11:11:59 +0200

    Added accordion for categories with hardcoded values

---
+++ auction-app-frontend/src/components/shop-page/categories-accordion/CategoriesAccordion.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './categoriesAccordion.css';
+
+const CategoriesAccordion = () => {
+  const [openedItems, setOpenedItems] = useState({});
+
+  const handleOpening = (event) => {
+    const category = event.target.dataset.category;
+    setOpenedItems((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
+
+  return (
+    <>
+      <div className='categories-accordion__wrapper'>
+        <h3>PRODUCT CATEGORIES</h3>
+        <div className='categories-accordion__category' data-category='fashion' onClick={handleOpening}>
+          <h4>Fashion</h4>
+          <p>{openedItems['fashion'] ? '-' : '+'}</p>
+        </div>
+        <div
+          className={`categories-accordion__subcategories ${
+            !openedItems['fashion'] && 'hidden'
+          }`}
+        >
+          <div className='categories-accordion__subcategory'>
+            <input type='checkbox' />
+            <p>Bag&nbsp;(50)</p>
+          </div>
+        </div>
+
+        <div className='categories-accordion__category' data-category='electronics' onClick={handleOpening}>
+          <h4>Electronics</h4>
+          <p>{openedItems['electronics'] ? '-' : '+'}</p>
+        </div>
+        <div
+          className={`categories-accordion__subcategories ${
+            !openedItems['electronics'] && 'hidden'
+          }`}
+        >
+          <div className='categories-accordion__subcategory'>
+            <input type='checkbox' />
+            <p>Phone&nbsp;(20)</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CategoriesAccordion;
