3e05268031feb04574b78d7eac2f5a6a8f3f252c
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-27 00:12:09 +0200

    Reimplemented repositories with necessary methods

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/CategoryRepository.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/CategoryRepository.java
@@ -1,21 +1,12 @@
 package com.atlantbh.auctionappbackend.repository;
 
 import com.atlantbh.auctionappbackend.model.Category;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query(value = "SELECT c.id, c.category_name, sc.id AS subcategory_id, sc.subcategory_name " +
-            "FROM category c " +
-            "JOIN subcategory sc ON c.id = sc.category_id " +
-            "WHERE c.id = :categoryId", nativeQuery = true )
-    Optional<Category> findCategoryByIdWithSubcategories(@Param("categoryId") Long categoryId);
 }
+
 
 
--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/ProductRepository.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/repository/ProductRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findAllByCategoryIdIn(long[] categoryId, Pageable paging);
+    Page<Product> findAllByCategory_Id(Long categoryId, Pageable paging);
 }
