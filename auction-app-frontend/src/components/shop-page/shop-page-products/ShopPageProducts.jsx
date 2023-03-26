870cfcff8b79fb0e178b4e2e7ba8d0ee061dbede
nemajeri <nemanja.jerinic832@gmail.com>
2023-03-26 18:14:42 +0200

    Added initial product components to mock the final look

---
+++ auction-app-frontend/src/components/shop-page/shop-page-products/ShopPageProducts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Product from  '../product/Product'
+
+const ShopPageProducts = () => {
+  return (
+    <>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+    </>
+  )
+}
+
+export default ShopPageProducts
