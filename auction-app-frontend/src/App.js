f4f97921371f2df65d22b002ebadfa5b398c348c
nemajeri <nemanja.jerinic832@gmail.com>
2023-05-23 09:19:51 +0200

    Changed imports to lazy load logic-heavy pages

--- auction-app-frontend/src/App.js
+++ auction-app-frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 import Navbar from './layout/navbar/Navbar';
 import Footer from './layout/footer/Footer';
@@ -16,23 +17,26 @@ import {
   sellerToAddItemPath,
 } from './utils/paths';
 import {
-  TermsAndCondPage,
   AboutPage,
   PrivacyPolicyPage,
-  ShopPage,
-  LandingPage,
-  ProductOverviewPage,
   LoginPage,
   RegisterPage,
-  MyAccountPage,
-  SellPage,
 } from './pages/index';
 import { Route, Routes } from 'react-router-dom';
 import { AppContextProvider } from './utils/AppContextProvider';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import  NotLoggedInRoute from './utils/routes/NotLoggedInRoute';
-import  LoggedInRoute from './utils/routes/LoggedInRoute';
+import NotLoggedInRoute from './utils/routes/NotLoggedInRoute';
+import LoggedInRoute from './utils/routes/LoggedInRoute';
+import LoadingSpinner from './components/loading-spinner/LoadingSpinner';
+
+const LandingPage = lazy(() => import('./pages/landing-page/LandingPage.js'));
+const ProductOverviewPage = lazy(() => import('./pages/product-overview-page/ProductOverviewPage.js'));
+const TermsAndCondPage = lazy(() => import('./pages/terms-and-cond/TermsAndCondPage.js'));
+const ShopPage = lazy(() => import('./pages/shop-page/ShopPage.js'));
+const MyAccountPage = lazy(() => import('./pages/my-account-page/MyAccountPage.js'));
+const SellPage = lazy(() => import('./pages/sell-page/SellPage.js'));
+
 
 function App() {
   return (
@@ -43,38 +47,90 @@ function App() {
         <Routes>
           <Route
             path={shopPagePathToProduct}
-            element={<ProductOverviewPage />}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <ProductOverviewPage />{' '}
+              </Suspense>
+            }
           />
-          <Route path={`${myAccountPath}/*`} element={
-            <LoggedInRoute>
-              <MyAccountPage />
-            </LoggedInRoute>
-          } />
-          <Route path={shopPagePath} element={<ShopPage />} />
-          <Route path={shopPagePathToCategory} element={<ShopPage />} />
+          <Route
+            path={`${myAccountPath}/*`}
+            element={
+              <LoggedInRoute>
+                <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                  <MyAccountPage />
+                </Suspense>
+              </LoggedInRoute>
+            }
+          />
+          <Route
+            path={shopPagePath}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <ShopPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path={shopPagePathToCategory}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <ShopPage />
+              </Suspense>
+            }
+          />
           <Route
             path={productOverviewPagePath}
-            element={<ProductOverviewPage />}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <ProductOverviewPage />
+              </Suspense>
+            }
           />
-          <Route path={loginPath} element={
-            <NotLoggedInRoute>
-              <LoginPage />
-            </NotLoggedInRoute>
-          } />
-          <Route path={registrationPath} element={
-            <NotLoggedInRoute>
-              <RegisterPage />
-            </NotLoggedInRoute>
-          } />
-          <Route path={landingPagePath} element={<LandingPage />} />
+          <Route
+            path={loginPath}
+            element={
+              <NotLoggedInRoute>
+                <LoginPage />
+              </NotLoggedInRoute>
+            }
+          />
+          <Route
+            path={registrationPath}
+            element={
+              <NotLoggedInRoute>
+                <RegisterPage />
+              </NotLoggedInRoute>
+            }
+          />
+          <Route
+            path={landingPagePath}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <LandingPage />
+              </Suspense>
+            }
+          />
           <Route path={aboutUsPath} element={<AboutPage />} />
-          <Route path={privacyPolicyPath} element={<TermsAndCondPage />} />
+          <Route
+            path={privacyPolicyPath}
+            element={
+              <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                <TermsAndCondPage />
+              </Suspense>
+            }
+          />
           <Route path={termsAndCondPath} element={<PrivacyPolicyPage />} />
-          <Route path={sellerToAddItemPath} element={
-            <LoggedInRoute>
-              <SellPage />
-            </LoggedInRoute>
-          } />
+          <Route
+            path={sellerToAddItemPath}
+            element={
+              <LoggedInRoute>
+                <Suspense fallback={<LoadingSpinner pageSpinner={true} />}>
+                  <SellPage />
+                </Suspense>
+              </LoggedInRoute>
+            }
+          />
         </Routes>
         <Footer />
       </AppContextProvider>
--- auction-app-frontend/src/pages/index.js
+++ auction-app-frontend/src/pages/index.js
@@ -1,10 +1,4 @@
-export { default as LandingPage } from './landing-page/LandingPage.js';
-export { default as ProductOverviewPage } from './product-overview-page/ProductOverviewPage.js';
 export { default as AboutPage } from './about-page/AboutPage.js';
 export { default as PrivacyPolicyPage } from './privacy-page/PrivacyPolicyPage.js';
-export { default as TermsAndCondPage } from './terms-and-cond/TermsAndCondPage.js';
-export { default as ShopPage } from './shop-page/ShopPage.js';
 export { default as RegisterPage } from './register-page/RegisterPage.js';
 export { default as LoginPage } from './login-page/LoginPage.js';
-export { default as MyAccountPage } from './my-account-page/MyAccountPage.js';
-export { default as SellPage } from './sell-page/SellPage.js';
