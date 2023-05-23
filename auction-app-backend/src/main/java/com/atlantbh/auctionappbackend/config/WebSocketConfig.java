ce85c07d7629281f88d3d5b9bb261115323853a9
nemajeri <nemanja.jerinic832@gmail.com>
2023-05-23 23:49:15 +0200

    Updated config to be able to validate jwt token from cookie

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/config/WebSocketConfig.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/config/WebSocketConfig.java
@@ -1,14 +1,32 @@
 package com.atlantbh.auctionappbackend.config;
 
+import com.atlantbh.auctionappbackend.service.TokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.atlantbh.auctionappbackend.utils.Constants.COOKIE_NAME;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@AllArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final TokenService tokenService;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,7 +36,26 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/notifications").withSockJS();
+        registry.addEndpoint("/notifications")
+                .withSockJS()
+                .setInterceptors(handshakeInterceptor());
+    }
+
+    @Bean
+    public HandshakeInterceptor handshakeInterceptor() {
+        return new HandshakeInterceptor() {
+
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                           WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                return true;
+            }
+
+            @Override
+            public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                       WebSocketHandler wsHandler, Exception exception) {
+            }
+        };
     }
 }
 
